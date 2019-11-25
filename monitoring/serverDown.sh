86a87b65cd6956b9a0a346f5f3b761b4bb5aa229
krishna <krishna.wattamwar@graymatrix.com>
2019-11-25 18:13:55 +0530

    update server down script

--- monitoring/serverDown.sh
+++ monitoring/serverDown.sh
@@ -2,10 +2,10 @@
 SERVERIP_UAT="Botler UAT"
 NOTIFYEMAIL="itsupport@graymatrix.com"
 #NOTIFYEMAIL1="krishna.wattamwar@graymatrix.com"
-COUNT_PROD=$(curl http://10.198.8.6 -k -s -f -o /dev/null --connect-timeout 30 && echo "SUCCESS" || echo "FAIL")
+COUNT_UAT=$(curl http://10.198.8.6 -k -s -f -o /dev/null --connect-timeout 30 && echo "SUCCESS" || echo "FAIL")
 MSG=""
 
-if [ $COUNT_PROD != "SUCCESS" ];
+if [ $COUNT_UAT != "SUCCESS" ];
 then
 	MSG="${MSG}$SERVERIP_UAT Server is down on $(date +" %d/%m/%Y at %R")\n"   
 else
