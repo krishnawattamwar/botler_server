156ce71021a6437e6ecf989aa28c990eee3c3040
krishna <krishna.wattamwar@graymatrix.com>
2020-03-02 17:48:32 +0530

    log_clear

--- monitoring/log_clear.sh
+++ monitoring/log_clear.sh
@@ -1,8 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 
 
-pm2 flush && pm2 restart all > /home/node/monitoring_logs/pm2_flush_$(date +"%Y_%m_%d-%I_%M_%p").log
+/usr/local/bin/pm2 flush >> /var/log/pm2_log/pm2_cleanup/pm2_flush_Daily_$(date +"%Y_%m_%d-%I_%M_%p").log && /usr/local/bin/pm2 restart all > /var/log/pm2_log/pm2_cleanup/pm2_restart_Daily_$(date +"%Y_%m_%d-%I_%M_%p").log
+
 sleep 10
-df -h > /home/node/monitoring_logs/disk_free_$(date +"%Y_%m_%d-%I_%M_%p").log
+
+df -h > /var/log/pm2_log/pm2_cleanup/disk_free_Daily_$(date +"%Y_%m_%d-%I_%M_%p").log
+
 sleep 10
-pm2 list > /home/node/monitoring_logs/pm2_list_$(date +"%Y_%m_%d-%I_%M_%p").log
+
+/usr/local/bin/pm2 list > /var/log/pm2_log/pm2_cleanup/pm2_list_Daily_$(date +"%Y_%m_%d-%I_%M_%p").log
