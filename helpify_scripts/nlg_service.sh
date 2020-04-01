02bb53c1d760572f0ceaac69a05e6ff94f6a558a
krishna <krishna.wattamwar@graymatrix.com>
2020-04-01 12:35:55 +0530

    adding script for helpify

--- helpify_scripts/nlg_service.sh
+++ helpify_scripts/nlg_service.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#port 5005
+#port 5056
 
 source /home/helpify.rasa/helpify.rasa.env/bin/activate
 echo "------------------------------" > /dev/null  2>&1
@@ -8,7 +8,8 @@ echo "------------------------------" > /dev/null  2>&1
 
 cd /home/helpify.rasa/www/
 
-echo -e "action server is starting now" > /dev/null  2>&1
-rasa run actions --actions actions --debug >> /home/helpify.rasa/www/logs/rasa_action-`/bin/date +\%Y-\%m-\%d-\%H-\%M-\%H-\%M-\%S`.log
-echo "done" > /dev/null  2>&1
+echo -e "action server is starting now"
+python nlg_server.py --domain domain >> /home/helpify.rasa/www/logs/rasa_nlg-`/bin/date +\%Y-\%m-\%d-\%H-\%M-\%H-\%M-\%S`.log
 
+echo "done" > /dev/null  2>&1
+ 
