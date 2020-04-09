6d115771879cf3ab7aecb66a7695197840ba2629
Krishna Wattamwar <krrishnagovt@gmail.com>
2020-04-09 05:34:04 +0000

    cashconnect action script changes

--- botler_scripts/cashconnect/service_action.sh
+++ botler_scripts/cashconnect/service_action.sh
@@ -9,11 +9,7 @@ echo "------------------------------" > /dev/null  2>&1
 cd /home/cashconnect_prod_rasa/www
 
 echo -e "action server is starting now" > /dev/null  2>&1
-<<<<<<< HEAD
-rasa run actions --actions actions -p 5063 >> /home/cashconnect_prod_rasa/www/logs/rasa_action-`/bin/date +\%Y-\%m-\%d-\%H-\%M-\%H-\%M-\%S`.log
-=======
-rasa run actions --actions actions -p 5063 --debug >> /home/cashconnect_prod_rasa/www/logs/rasa_action-`/bin/date +\%Y-\%m-\%d-\%Hh-\%Mm-\%Ss`.log'
->>>>>>> ae65bdb85a7fdf63b2ba44d8ca0647bde7b7f569
+rasa run actions --actions actions -p 5063 --debug >> /home/cashconnect_prod_rasa/www/logs/rasa_action-`/bin/date +\%Y-\%m-\%d-\%Hh-\%Mm-\%Ss`.log
 
 echo "done" > /dev/null  2>&1
 
